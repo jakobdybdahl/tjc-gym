3e99df194f46763420d7f08a468de3af307d5446
Jakob Dybdahl <dybdahl@smukand.dk>
2021-12-19 11:35:27 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ from setuptools import setup
 
 setup(
     name="tjc-gym",
-    version="0.0.1",
+    version="0.0.2",
     description="OpenAI Gym environment of Traffic Junction with continuous action space",
-    long_description_content_type='text/markdown',
-    long_description=open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8').read(),
-    url='https://github.com/jakobdybdahl/tjc-gym',
+    long_description_content_type="text/markdown",
+    long_description=open(path.join(path.abspath(path.dirname(__file__)), "README.md"), encoding="utf-8").read(),
+    url="https://github.com/jakobdybdahl/tjc-gym",
     author="Jakob Dybdahl, Rasmus Thorsen",
     author_email="dybdahl@smukand.dk",
-    license='MIT License',
+    license="MIT License",
     packages=setuptools.find_packages(),
     install_requires=["gym==0.21.0", "numpy>=1.21.4", "pyglet>=1.5.21"],
     classifiers=[
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9'
-      ],
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
